95ce9343f254da6e65fef1b289d43e1f48cacc57
Jordan Moats <moats.jordan@gmail.com>
2024-11-13 19:16:19 -0500

    adds day 8 and 9. part a for both, part b for 9

--- 2020/day8.py
+++ 2020/day8.py
@@ -3,32 +3,70 @@ with open('2020/input/8.txt') as file:
 
 def a():
     lines = content.split('\n')
+    instructions = []
+    for line in lines:
+        op, arg = line.strip().split(' ')
+        instructions.append((op, int(arg)))
 
     accumulator = 0
-    visited_lines = []
+    visited_lines = set()
     i = 0
     while i not in visited_lines:
-        visited_lines.append(i)         
-
-        line = lines[i]
-        line_pieces = line.split(' ')
-        operation = line_pieces[0]
-        argument = int(line_pieces[1])
-
-        print('', line, operation, argument, sep='\n')
+        visited_lines.add(i)         
         
-        if operation == 'nop':
+        op, arg = instructions[i]
+
+        if op == 'nop':
             i += 1
-        elif operation == 'acc':
+        elif op == 'acc':
             i += 1
-            accumulator += argument
+            accumulator += arg
         else:
-            i += argument
-
-
+            i += arg
 
     print(f'accumulator: {accumulator}')
 
-a()
+# does not work
+def b():
+    lines = content.split('\n')
+    instructions = []
+    for line in lines:
+        op, arg = line.strip().split(' ')
+        instructions.append((op, int(arg)))
+    
+    # If I work backwards, I can get a list of target rows, then check if flipping any row gets you to the target?
+    target_rows = set()
+    j = len(instructions) - 1
+    while True:
+        op, arg = instructions[j]
+        
+        if op == 'jmp' and arg <= 0:
+            break
+        else:
+            target_rows.add(j)
+        
+    accumulator = 0
+    i = 0
+    while True:
+        if i >= len(instructions):
+            break
+        op, arg = instructions[i]
 
+        if op == 'nop':
+            if i + arg in target_rows:
+                i += arg
+            else:
+                i += 1
+        elif op == 'jmp':
+            if i + 1 in target_rows:
+                i += 1
+            else:
+                i += arg
+        else:
+            accumulator += arg
+            i += 1
+    print('accumulator: ', accumulator)
+
+a()
+b()
 
